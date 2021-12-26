adb29ca0bc4aa85ac65215b6ed7adf26cf019a81
Yoshihiro Tanaka <contact@cordea.jp>
2021-12-26 14:04:46 +0900

    implement page

--- lib/user.dart
+++ lib/user.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
-class UserPage extends StatelessWidget {
-  const UserPage({Key? key}) : super(key: key);
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key, required this.following}) : super(key: key);
+
+  final bool following;
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  late bool following;
+
+  @override
+  void initState() {
+    super.initState();
+    following = widget.following;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('User')),
-      body: Center(),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              following = !widget.following;
+            });
+          },
+          child: const Text('Follow'),
+        ),
+      ),
     );
   }
 }

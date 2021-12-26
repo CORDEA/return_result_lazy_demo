fa2a9a7ebb920aa96730a2a9f33ce49b574a4bf4
Yoshihiro Tanaka <contact@cordea.jp>
2021-12-26 14:05:03 +0900

    add buttons

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'user.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -7,7 +9,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
-      body: Center(),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UserPage(following: true),
+                ),
+              );
+              print(result);
+            },
+            child: const Text('Open'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Open'),
+          ),
+        ],
+      ),
     );
   }
 }
